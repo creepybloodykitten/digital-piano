463986ec617155103a6efc1bd56c6e0f02b35995
creepybloodykitten <nikita2005420054@gmail.com>
2026-05-01 00:13:43 +0700

    add memory and wavetable base

--- voice.sv
+++ voice.sv
@@ -5,36 +5,22 @@ module piano_voice(
     output wire signed [15:0] audio_out // output of one voice
 );
     //  Phase accumulator (unique for each voice)
-    reg [15:0] phase_acc = 0;
+    reg [31:0] phase_acc = 0;
     always @(posedge clk_lrck) begin
         if (freq == 0) phase_acc <= 0;
-        else phase_acc <= phase_acc + freq;
+        else phase_acc <= phase_acc + {freq, 16'b0};
     end
 
     // Sine table (unique for each voice)
-    wire [4:0] rom_index = phase_acc[15:11];
-    reg signed [15:0] sine_wave;
-    always @(*) begin
-        case(rom_index)
-            5'd0 : sine_wave =  16'd0;     5'd1 : sine_wave =  16'd6242;
-            5'd2 : sine_wave =  16'd12245; 5'd3 : sine_wave =  16'd17760;
-            5'd4 : sine_wave =  16'd22627; 5'd5 : sine_wave =  16'd26607;
-            5'd6 : sine_wave =  16'd29560; 5'd7 : sine_wave =  16'd31384;
-            5'd8 : sine_wave =  16'd32000; 5'd9 : sine_wave =  16'd31384;
-            5'd10: sine_wave =  16'd29560; 5'd11: sine_wave =  16'd26607;
-            5'd12: sine_wave =  16'd22627; 5'd13: sine_wave =  16'd17760;
-            5'd14: sine_wave =  16'd12245; 5'd15: sine_wave =  16'd6242;
-            5'd16: sine_wave =  16'd0;     5'd17: sine_wave = -16'd6242;
-            5'd18: sine_wave = -16'd12245; 5'd19: sine_wave = -16'd17760;
-            5'd20: sine_wave = -16'd22627; 5'd21: sine_wave = -16'd26607;
-            5'd22: sine_wave = -16'd29560; 5'd23: sine_wave = -16'd31384;
-            5'd24: sine_wave = -16'd32000; 5'd25: sine_wave = -16'd31384;
-            5'd26: sine_wave = -16'd29560; 5'd27: sine_wave = -16'd26607;
-            5'd28: sine_wave = -16'd22627; 5'd29: sine_wave = -16'd17760;
-            5'd30: sine_wave = -16'd12245; 5'd31: sine_wave = -16'd6242;
-            default: sine_wave = 16'd0;
-        endcase
-    end
+    wire [9:0] rom_addr = phase_acc[31:22];
+    wire signed [15:0] raw_wave;
+	 
+	 wave_rom rom_inst (
+        .address(rom_addr),
+        .clock(clk_lrck),
+        .q(raw_wave)
+    );
+	 
 
     // Envelope (unique for each voice)
     reg [15:0] envelope = 0;
@@ -60,7 +46,7 @@ module piano_voice(
 
     // VCA
     wire signed [16:0] env_signed = {1'b0, envelope};
-    wire signed [32:0] mixed_audio = sine_wave * env_signed;
+    wire signed [32:0] mixed_audio = raw_wave * env_signed;
     assign audio_out = mixed_audio[31:16];
 
 endmodule
