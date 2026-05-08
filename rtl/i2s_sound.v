0912909f327ec588a0d8a39971b12be14023d28c
creepybloodykitten <nikita2005420054@gmail.com>
2026-05-08 22:03:55 +0700

    add 2 octaves via midi

--- rtl/i2s_sound.v
+++ rtl/i2s_sound.v
@@ -39,45 +39,6 @@ module Simple_I2S_Tone(
         .out_trig(midi_trig)
     );
 
-//    reg [15:0] target_freq;
-//    always @(*) begin
-//        case (midi_note)
-//            48: target_freq = 16'd176;
-//            49: target_freq = 16'd186;
-//            50: target_freq = 16'd197;
-//            51: target_freq = 16'd209;
-//            52: target_freq = 16'd221;
-//            53: target_freq = 16'd234;
-//            54: target_freq = 16'd248;
-//            55: target_freq = 16'd263;
-//            56: target_freq = 16'd279;
-//            57: target_freq = 16'd295;
-//            58: target_freq = 16'd313;
-//            59: target_freq = 16'd331;
-//            default: target_freq = 0;
-//        endcase
-//    end
-
-//    reg [15:0] target_freq;
-//    always @(*) begin
-//        case (midi_note)
-//            48: target_freq = 16'd350;
-//            50: target_freq = 16'd393;
-//            52: target_freq = 16'd442;
-//            default: target_freq = 0;
-//        endcase
-//    end
-
-
-//    wire signed [15:0] voice_out;
-//    piano_voice voice1 (
-//        .clk_lrck(aud_lrck),
-//        .freq(target_freq),
-//        .key_pressed(midi_on),   
-//        .velocity(midi_vel),     
-//		//.velocity(8'd127),
-//        .audio_out(voice_out)
-//    );
 
 	 reg [6:0] v_note [0:9]; // Какую ноту играет каждый из 10 голосов
     reg [7:0] v_vel  [0:9]; // С какой силой нажали
@@ -89,46 +50,44 @@ module Simple_I2S_Tone(
     always @(posedge clk_50) begin
         if (midi_trig) begin
             if (midi_on) begin
-                // ПРИШЛА КОМАНДА "НАЖАТЬ" (Note On)
+                // пришла команда нажать note on
                 allocated = 0;
                 for (i = 0; i < 10; i = i + 1) begin
-                    // Ищем первый свободный голос (который не нажат)
+                    // Ищем первый свободный голос 
                     if (!v_on[i] && !allocated) begin
                         v_on[i]   <= 1'b1;
                         v_note[i] <= midi_note[6:0];
                         v_vel[i]  <= midi_vel;
-                        allocated = 1; // Заняли голос, дальше не ищем
+                        allocated = 1; // заняли голос, дальше не ищем
                     end
                 end
             end else begin
-                // ПРИШЛА КОМАНДА "ОТПУСТИТЬ" (Note Off)
+                // пришла команда отпустить note Off
                 for (i = 0; i < 10; i = i + 1) begin
-                    // Ищем голос, который играет ИМЕННО ЭТУ ноту
+                    // ищем голос, который играет ИМЕННО ЭТУ ноту
                     if (v_on[i] && (v_note[i] == midi_note[6:0])) begin
-                        v_on[i] <= 1'b0; // Начинаем затухание (Release)
+                        v_on[i] <= 1'b0; // начинаем затухание 
                     end
                 end
             end
         end
     end
 
-    // ==========================================
-    // 4. ГЕНЕРАЦИЯ 10 ГОЛОСОВ (МАССИВ МОДУЛЕЙ)
-    // ==========================================
-    wire signed [15:0] v_out[0:9]; // Выходы звука от каждого голоса
+
+    wire signed [15:0] v_out[0:9]; // выходы звука от каждого голоса
 
     genvar g;
     generate
         for (g = 0; g < 10; g = g + 1) begin : voices
             wire [15:0] freq_wire;
             
-            // Переводчик "Нота -> Частота" для каждого голоса
+            // связующее звено нота -> частота
             note_to_freq n2f_inst (
                 .note(v_note[g]),
                 .freq(freq_wire)
             );
 
-            // Сам синтезатор (твоя функция)
+           
             piano_voice voice_inst (
                 .clk_lrck(aud_lrck),
                 .freq(freq_wire),
@@ -139,22 +98,20 @@ module Simple_I2S_Tone(
         end
     endgenerate
 
-    // ==========================================
-    // 5. МИКШЕР (СЛОЖЕНИЕ ЗВУКА)
-    // ==========================================
+	 //--------------
+	 //mixer 
     wire signed [19:0] mix_sum;
-    // Складываем все 10 голосов в 20-битную переменную, чтобы не было переполнения
+    
     assign mix_sum = $signed(v_out[0]) + $signed(v_out[1]) + 
                      $signed(v_out[2]) + $signed(v_out[3]) + 
                      $signed(v_out[4]) + $signed(v_out[5]) + 
                      $signed(v_out[6]) + $signed(v_out[7]) + 
                      $signed(v_out[8]) + $signed(v_out[9]);
 
-    // Уменьшаем громкость (сдвиг вправо на 3 бита = деление на 8)
-    // Это гарантирует, что 10 нажатых клавиш не "порвут" динамики хрипом
+    // уменьшаем громкость (сдвиг вправо на 3 бита = деление на 8)
     wire signed[19:0] mix_scaled = mix_sum >>> 1; 
 
-    // Лимитер (Сатурация): защищаем динамики от треска при аккордах из 10 нот
+
     wire [15:0] final_audio = (mix_scaled > 20'sd32767)  ? 16'sd32767 :
                               (mix_scaled < -20'sd32768) ? -16'sd32768 :
                               mix_scaled[15:0];
